85fa22a916631d597c20076621f359c0d965922e
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-09-22 23:54:51 -0300

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -1,8 +1,14 @@
+import time
 import pyautogui
 
 
 pyautogui.alert('O código de automação vai começar....')
-# Abrindo o edge
+pyautogui.PAUSE = 0.5 
+# Abrindo o edge e realizando 1° pesquisa e atualizando a aba do navegador
 pyautogui.press('win')
 pyautogui.write('edge')
 pyautogui.press('enter')
+pyautogui.write('anime one peace')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
