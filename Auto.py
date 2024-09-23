2b44f5def34efa4859fe9c16b9ebcc71f46f4ecc
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-09-23 00:02:27 -0300

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -3,7 +3,7 @@ import pyautogui
 
 
 pyautogui.alert('O código de automação vai começar....')
-pyautogui.PAUSE = 0.5 
+pyautogui.PAUSE = 1.0
 # Abrindo o edge e realizando 1° pesquisa e atualizando a aba do navegador
 pyautogui.press('win')
 pyautogui.write('edge')
@@ -12,3 +12,263 @@ pyautogui.write('anime one peace')
 pyautogui.press('enter')
 time.sleep(1)
 pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 2° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Dragonball')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 3° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Dathnote')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 4° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Another')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 6° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime ATTACK ON TITAN')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 7° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Tatame galaxy')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 8° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime The Faraway Paladin')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 9° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime link click')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 10° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime That Time I Got Reincarnated as a Slime')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 11° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Sonny boy')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 12° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime sword art online sao')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 13° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime The rinsing of the shield hero')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 14° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime RE: zero starting life in another world')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 15° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Overlord')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 16° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime no game no life ')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 17° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Fullmetal Alchemist e Fullmetal Alchemist: Brotherhood')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 18° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Cavaleiros do Zodíaco')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 19° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Samurai x')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 20° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Cowboy bebop')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 21° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Pysico pes')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 22° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Tokyo ghoul ')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 23° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime hunter x hunter')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 24° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime bleach')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 25° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime mosters')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 26° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Neon Genesis Evangelion')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 27° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime ghost in the shell')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 28° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Ergo proxy')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 29° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Suzumiya Haruhi no Yuuutsu')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 30° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime Stein Gate')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('ctrl','w')
+
+# Abrindo o edge e realizando 31° pesquisa e atualizando a aba do navegador
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('anime The Girl Who Leapt Through Time')
+pyautogui.press('enter')
+
