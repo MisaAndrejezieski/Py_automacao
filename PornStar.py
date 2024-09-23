db20e5347794a4d37722f9c5a5f0e9b943dcfea7
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-09-23 01:45:56 -0300

    Create PornStar.py

---
+++ PornStar.py
@@ -0,0 +1,33 @@
+import time
+import pyautogui
+
+# Lista de URLs para acessar
+urls = [
+    'https://sites.google.com/view/misa-misa/home',
+    'https://sites.google.com/view/deusadoshi/nicole-doshi',
+    'https://sites.google.com/view/misa-misa/projetos',
+    'https://sites.google.com/view/misa-misa/filmes',
+    'https://sites.google.com/view/misa-misa/t-i',
+    'https://sites.google.com/view/misa-misa/windows'
+]
+
+pyautogui.alert('O código de automação de pesquisa no Edge vai começar....')
+pyautogui.PAUSE = 0.5
+
+# Abrindo o Edge e realizando a primeira pesquisa
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.hotkey('ctrl', 't')
+
+# Iniciando o laço de repetição para acessar cada URL
+for url in urls:
+    pyautogui.write(url)
+    pyautogui.press('enter')
+    time.sleep(2)  # Espera 5 segundos para carregar a página
+    pyautogui.hotkey('ctrl', 'w')
+    pyautogui.hotkey('ctrl', 't')
+
+# Fechar a janela atual com Alt + F4
+pyautogui.hotkey('alt', 'f4')
